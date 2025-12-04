68f5b1f1ecca7004afc8ca73142808e1a6ab9c05
helgaathifah05@gmail.com <helgaathifah05@gmail.com>
2025-12-04 16:24:45 +0700

    Debug - by Helga

--- resources/views/partials/doc-header.blade.php
+++ resources/views/partials/doc-header.blade.php
@@ -1,12 +1,11 @@
-<!-- Header bagian atas untuk menampilkan nama surat dan info halaman -->
-<div class="pv-header">
+<div class="pv-header" style="position: sticky; top: 50px; z-index: 10;">
     <div class="pv-name">{{ $judulSurat ?? 'Nama surat' }}</div>
     
     <div class="doc-pagination">
         {{-- TAMBAHKAN ID="curr_page" DAN ID="total_pages" DI SINI --}}
-        Halaman 
-        <span id="curr_page">{{ $currentPage }}</span> 
-        dari 
+        Halaman
+        <span id="curr_page">{{ $currentPage }}</span>
+        dari
         <span id="total_pages">{{ $totalPages }}</span>
     </div>
 </div>
